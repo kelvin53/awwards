e3fc7b05c3e0d08cf7012520eebc582d7a3a9aab
Moringa School Access <geshkoskei53@gmail.com>
2020-08-30 13:48:56 +0300

    create model class profile

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo=models.ImageField(upload_to='profiles',blank=True)
+    user = models.OneToOneField(User,null = True,on_delete=models.CASCADE,related_name = "profile")
+    bio=models.TextField(blank=True,null=True)
+    contact=models.EmailField(blank=True,null=True)
+
+    def __str__(self):
+        return self.user
+
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
