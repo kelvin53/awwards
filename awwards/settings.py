c4aca35b8310ba32939d2f08f9ee798f2dc9225f
Moringa School Access <geshkoskei53@gmail.com>
2020-08-30 14:39:52 +0300

    update settings

--- awwards/settings.py
+++ awwards/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
+    'awards.apps.AwardsConfig',
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
