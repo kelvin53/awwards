e733aa9c108a740716e728ff75e4abdd5878753b
Moringa School Access <geshkoskei53@gmail.com>
2020-08-30 13:42:09 +0300

    create login view

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    projects=Project.get_projects()
+
+    return render(request,'index.html',{"projects":projects})
+
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.profile = current_user
+            project.save()
+        return redirect('index')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form": form})
+
