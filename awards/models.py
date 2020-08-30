020b477751f146b3b01f1f1cd89c01f0ae15a0d2
Moringa School Access <geshkoskei53@gmail.com>
2020-08-30 13:49:50 +0300

    create model class project

--- awards/models.py
+++ awards/models.py
@@ -14,3 +14,33 @@ class Profile(models.Model):
         self.save()
     def delete_profile(self):
         self.delete()
+
+
+class Project(models.Model):
+    title=models.CharField(max_length =30)
+    image=models.ImageField(upload_to ='projects/', blank = True)
+    description=models.TextField(blank=True,null=True)
+    profile=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    link=models.TextField(validators=[URLValidator()],blank=True)
+    design=models.PositiveIntegerField(choices=list(zip(range(1, 11), range(1, 11))), default=1)
+    usability=models.PositiveIntegerField(choices=list(zip(range(1, 11), range(1, 11))), default=1)
+    content=models.PositiveIntegerField(choices=list(zip(range(1, 11), range(1, 11))), default=1)
+
+    
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_projects(cls):
+        projects=cls.objects.all()
+        return projects
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
