8d6c9e63f3e189f31c5285de900a4c066da047db
Moringa School Access <geshkoskei53@gmail.com>
2020-08-30 13:50:48 +0300

    update model import section

--- awards/models.py
+++ awards/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import URLValidator,MaxValueValidator,MinValueValidator
+from tinymce.models import HTMLField
+
 
 # Create your models here.
 class Profile(models.Model):
